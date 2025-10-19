4ffd8cab343df83ec6dc29718cf71bbb472ec55b
the-real-nox <the-real-nox@outlook.com>
2025-10-19 15:07:28 +0200

    Not abel to ts-files yet.

--- lua/marco/init.lua
+++ lua/marco/init.lua
@@ -2,7 +2,13 @@ local M = {}
 
 function M.compile()
     local current_file = vim.fn.expand("%:p")
-    local output = vim.fn.system({ "ts-node", "lib/src/main.ts", current_file })
+    local output = vim.fn.system({ "npx", "ts-node", "./lib/src/main.ts", current_file })
+    vim.notify(output)
+end
+
+function M.install()
+    vim.notify("Test")
+    local output = vim.fn.system({ "npm", "i"})
     vim.notify(output)
 end
 
